5baadeadbb8d9f9d5c0e7585de3e37fed702edb6
Eric Rostedt <rostedteric@gmail.com>
2025-09-06 16:40:12 +0200

    move out image

---
+++ src/image.hpp
@@ -0,0 +1,79 @@
+#include <cmath>
+#include <cstdint>
+#include <vector>
+
+using u8 = uint8_t;
+using u32 = uint32_t;
+
+struct RGB
+{
+    u8 r;
+    u8 g;
+    u8 b;
+};
+
+class Image
+{
+    using iterator = std::vector<RGB>::iterator;
+    using const_iterator = std::vector<RGB>::const_iterator;
+
+  public:
+    Image(u32 width, u32 height) : m_width(width), m_height(height), pixels(width * height)
+    {
+    }
+
+    inline u32 width() const
+    {
+        return m_width;
+    }
+
+    inline u32 height() const
+    {
+        return m_height;
+    }
+
+    inline RGB &operator[](u32 i)
+    {
+        return pixels[i];
+    }
+
+    inline RGB &operator[](u32 x, u32 y)
+    {
+        return operator[](y * m_width + x);
+    }
+
+    inline const RGB &operator[](u32 i) const
+    {
+        return pixels[i];
+    }
+
+    inline const RGB &operator[](u32 x, u32 y) const
+    {
+        return operator[](y * m_width + x);
+    }
+
+    inline iterator begin()
+    {
+        return std::begin(pixels);
+    }
+
+    inline iterator end()
+    {
+        return std::end(pixels);
+    }
+
+    inline const_iterator begin() const
+    {
+        return std::cbegin(pixels);
+    }
+
+    inline const_iterator end() const
+    {
+        return std::cend(pixels);
+    }
+
+  private:
+    u32 m_width;
+    u32 m_height;
+    std::vector<RGB> pixels;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <vector>
+
+#include "image.hpp"
 
 using u32 = uint32_t;
-using u8 = uint8_t;
 using f32 = float;
 
 template <typename T> struct Point
@@ -60,79 +60,6 @@ BoundingBox TriangleBoundingBox(const Triangle &t)
     f32 bottom = max(max(t.p1.x, t.p2.x), t.p3.x);
     return {{FloorToU32(left), FloorToU32(top)}, {CeilToU32(right), CeilToU32(bottom)}};
 }
-
-struct RGB
-{
-    u8 r;
-    u8 g;
-    u8 b;
-};
-
-class Image
-{
-    using iterator = std::vector<RGB>::iterator;
-    using const_iterator = std::vector<RGB>::const_iterator;
-
-  public:
-    Image(u32 width, u32 height) : m_width(width), m_height(height), pixels(width * height)
-    {
-    }
-
-    inline u32 width() const
-    {
-        return m_width;
-    }
-
-    inline u32 height() const
-    {
-        return m_height;
-    }
-
-    inline RGB &operator[](u32 i)
-    {
-        return pixels[i];
-    }
-
-    inline RGB &operator[](u32 x, u32 y)
-    {
-        return operator[](y * m_width + x);
-    }
-
-    inline const RGB &operator[](u32 i) const
-    {
-        return pixels[i];
-    }
-
-    inline const RGB &operator[](u32 x, u32 y) const
-    {
-        return operator[](y * m_width + x);
-    }
-
-    inline iterator begin()
-    {
-        return std::begin(pixels);
-    }
-
-    inline iterator end()
-    {
-        return std::end(pixels);
-    }
-
-    inline const_iterator begin() const
-    {
-        return std::cbegin(pixels);
-    }
-
-    inline const_iterator end() const
-    {
-        return std::cend(pixels);
-    }
-
-  private:
-    u32 m_width;
-    u32 m_height;
-    std::vector<RGB> pixels;
-};
 
 void DrawTriangle(Image &image, const Triangle &t, RGB color)
 {

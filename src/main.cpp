edd70cb84c8d5ece9010ca86f48ab9ddb1dea0d5
Eric Rostedt <rostedteric@gmail.com>
2025-09-07 11:26:55 +0200

    Speed up using deltas

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cassert>
 #include <cmath>
 #include <iostream>
 
@@ -59,32 +60,6 @@ BoundingBox TriangleBoundingBox(const Triangle &t)
     return {{FloorToU32(left), FloorToU32(top)}, {CeilToU32(right), CeilToU32(bottom)}};
 }
 
-inline f32 Edge(PointF32 p1, PointF32 p2, f32 x, f32 y)
-{
-    return (x - p1.x) * (p2.y - p1.y) - (y - p1.y) * (p2.x - p1.x);
-}
-
-void DrawTriangle(Image &image, const Triangle &t, RGB color)
-{
-    BoundingBox box = TriangleBoundingBox(t);
-    for (u32 y = box.top_left.y; y < box.bottom_right.y; ++y)
-    {
-        for (u32 x = box.top_left.x; x < box.bottom_right.x; ++x)
-        {
-            f32 px = (f32)x + 0.5f;
-            f32 py = (f32)y + 0.5f;
-            f32 w0 = Edge(t.p2, t.p3, (f32)px, (f32)py);
-            f32 w1 = Edge(t.p3, t.p1, (f32)px, (f32)py);
-            f32 w2 = Edge(t.p1, t.p2, (f32)px, (f32)py);
-
-            if (w0 >= 0 && w1 >= 0 && w2 >= 0)
-            {
-                image[x, y] = color;
-            }
-        }
-    }
-}
-
 inline void WritePixel(const RGB &color)
 {
     std::cout << (int)color.r << ' ' << (int)color.g << ' ' << (int)color.b << '\n';
@@ -98,24 +73,68 @@ void WriteImage(const Image &image)
     for_each(image, WritePixel);
 }
 
+inline f32 Edge(PointF32 p1, PointF32 p2, PointF32 p3)
+{
+    return (p3.y - p1.y) * (p2.x - p1.x) - (p3.x - p1.x) * (p2.y - p1.y);
+}
+
+inline f32 Edge(const Triangle &t)
+{
+    return Edge(t.p1, t.p2, t.p3);
+}
+
+void DrawTriangle(Image &image, const Triangle &t, RGB color)
+{
+    assert(Edge(t) >= 0.0f && "Triangle must be Screen space CCW");
+    BoundingBox box = TriangleBoundingBox(t);
+
+    f32 left = (f32)box.top_left.x + 0.5f;
+    f32 top = (f32)box.top_left.y + 0.5f;
+
+    f32 w0 = Edge(t.p2, t.p3, {left, top});
+    f32 w1 = Edge(t.p3, t.p1, {left, top});
+    f32 w2 = Edge(t.p1, t.p2, {left, top});
+
+    f32 w0_dx = t.p2.y - t.p3.y;
+    f32 w0_dy = t.p3.x - t.p2.x;
+
+    f32 w1_dx = t.p3.y - t.p1.y;
+    f32 w1_dy = t.p1.x - t.p3.x;
+
+    f32 w2_dx = t.p1.y - t.p2.y;
+    f32 w2_dy = t.p2.x - t.p1.x;
+
+    for (u32 y = box.top_left.y; y < box.bottom_right.y; ++y)
+    {
+        f32 w0i = w0;
+        f32 w1i = w1;
+        f32 w2i = w2;
+
+        for (u32 x = box.top_left.x; x < box.bottom_right.x; ++x)
+        {
+            if (w0i >= 0 && w1i >= 0 && w2i >= 0)
+            {
+                image[x, y] = color;
+            }
+            w0i += w0_dx;
+            w1i += w1_dx;
+            w2i += w2_dx;
+        }
+        w0 += w0_dy;
+        w1 += w1_dy;
+        w2 += w2_dy;
+    }
+}
+
 int main()
 {
     Triangle t1{
-
         {200, 100},
-        {100, 300},
         {300, 300},
-    };
-
-    Triangle t2{
-
-        {300, 100},
         {100, 300},
-        {300, 300},
     };
 
     Image image(400, 400);
     DrawTriangle(image, t1, {255, 0, 0});
-    DrawTriangle(image, t2, {0, 255, 0});
     WriteImage(image);
 }

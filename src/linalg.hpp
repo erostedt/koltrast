25b0cb75770266d4b08c5d9f1a110ec8badf093a
Eric Rostedt <rostedteric@gmail.com>
2025-09-17 13:12:41 +0200

    add concepts

--- src/linalg.hpp
+++ src/linalg.hpp
@@ -1,8 +1,34 @@
 #include <algorithm>
 #include <array>
 #include <cmath>
+#include <concepts>
 #include <functional>
 #include <numeric>
+
+template <typename T, typename U>
+concept Addable = requires(T a, U b) {
+    { a + b };
+};
+
+template <typename T, typename U>
+concept Subtractable = requires(T a, U b) {
+    { a - b };
+};
+
+template <typename T>
+concept Negatable = requires(T a) {
+    { -a } -> std::same_as<T>;
+};
+
+template <typename T, typename U>
+concept Multiplicable = requires(T a, U b) {
+    { a * b };
+};
+
+template <typename T, typename U>
+concept Divisable = requires(T a, U b) {
+    { a / b };
+};
 
 template <typename T, typename U, size_t N, typename BinaryOperation>
 constexpr auto Elementwise(const std::array<T, N> &left, const std::array<U, N> &right, BinaryOperation op)
@@ -34,7 +60,9 @@ constexpr auto Elementwise(T left, const std::array<U, N> &right, BinaryOperatio
     return out;
 }
 
-template <typename T, size_t N> class _Vector
+template <typename T, size_t N>
+    requires std::is_signed_v<T>
+class Vector
 {
     using value_type = std::array<T, N>::value_type;
     using pointer = std::array<T, N>::pointer;
@@ -49,13 +77,13 @@ template <typename T, size_t N> class _Vector
     using const_reverse_iterator = std::array<T, N>::const_reverse_iterator;
 
   public:
-    template <typename... U> constexpr _Vector(U... elems) : elements{elems...}
+    template <typename... U> constexpr Vector(U... elems) : elements{elems...}
     {
         static_assert(sizeof...(U) == N, "Wrong number of elements");
         static_assert((std::is_same_v<U, T> && ...), "All arguments must be exactly T");
     }
 
-    constexpr _Vector(const std::array<T, N> &arr) : elements(arr)
+    constexpr Vector(const std::array<T, N> &arr) : elements(arr)
     {
     }
 
@@ -79,12 +107,14 @@ template <typename T, size_t N> class _Vector
         return std::end(elements);
     }
 
-    [[nodiscard]] constexpr inline bool operator==(const _Vector &v) const noexcept
+    [[nodiscard]] constexpr inline bool operator==(const Vector &v) const noexcept
+        requires std::equality_comparable<T>
     {
-        return std::equal(begin(), end(), std::begin(v));
+        return elements == v.elements;
     }
 
-    [[nodiscard]] constexpr inline bool operator!=(const _Vector &v) const noexcept
+    [[nodiscard]] constexpr inline bool operator!=(const Vector &v) const noexcept
+        requires std::equality_comparable<T>
     {
         return !(*this == v);
     }
@@ -94,61 +124,106 @@ template <typename T, size_t N> class _Vector
         return N;
     }
 
-    [[nodiscard]] constexpr inline T dot(const _Vector &v) const noexcept
+    [[nodiscard]] constexpr inline T dot(const Vector &v) const noexcept
+        requires Multiplicable<T, T> && Addable<T, T>
     {
         return std::inner_product(this->begin(), this->end(), std::begin(v), T{});
     }
 
     [[nodiscard]] constexpr inline T squared_length() const noexcept
+        requires std::floating_point<T>
     {
         return this->dot(*this);
     }
 
     [[nodiscard]] constexpr inline T length() const noexcept
+        requires std::floating_point<T>
     {
         return std::sqrt(squared_length());
+    }
+
+    [[nodiscard]] constexpr inline T x() const noexcept
+        requires(N >= 1 && N <= 4)
+    {
+        return elements[0];
+    }
+
+    [[nodiscard]] constexpr inline T y() const noexcept
+        requires(N >= 2 && N <= 4)
+    {
+        return elements[1];
+    }
+
+    [[nodiscard]] constexpr inline T z() const noexcept
+        requires(N >= 3 && N <= 4)
+    {
+        return elements[2];
+    }
+
+    [[nodiscard]] constexpr inline T w() const noexcept
+        requires(N == 4)
+    {
+        return elements[3];
+    }
+
+    [[nodiscard]] constexpr inline Vector cross(const Vector &v) const noexcept
+        requires(N == 3) && Multiplicable<T, T> && Subtractable<T, T>
+    {
+        const auto &u = *this;
+        const auto x = u.y() * v.z() - u.z() * v.y();
+        const auto y = u.z() * v.x() - u.x() * v.z();
+        const auto z = u.x() * v.y() - u.y() * v.x();
+        return {x, y, z};
     }
 
     std::array<T, N> elements{};
 };
 
 template <typename T, size_t N>
-[[nodiscard]] constexpr inline _Vector<T, N> operator+(const _Vector<T, N> &left, const _Vector<T, N> &right) noexcept
+[[nodiscard]] constexpr inline Vector<T, N> operator+(const Vector<T, N> &left, const Vector<T, N> &right) noexcept
+    requires Addable<T, T>
 {
     return Elementwise(left.elements, right.elements, std::plus<T>{});
 }
 
 template <typename T, size_t N>
-[[nodiscard]] constexpr inline _Vector<T, N> operator+(const _Vector<T, N> &left, const T &right) noexcept
+[[nodiscard]] constexpr inline Vector<T, N> operator+(const Vector<T, N> &left, const T &right) noexcept
+    requires Addable<T, T>
 {
     return Elementwise(left.elements, right, std::plus<T>{});
 }
 
 template <typename T, size_t N>
-[[nodiscard]] constexpr inline _Vector<T, N> operator+(const T &left, const _Vector<T, N> &right) noexcept
+[[nodiscard]] constexpr inline Vector<T, N> operator+(const T &left, const Vector<T, N> &right) noexcept
+    requires Addable<T, T>
 {
     return Elementwise(left, right.elements, std::plus<T>{});
 }
 
 template <typename T, size_t N>
-[[nodiscard]] constexpr inline _Vector<T, N> operator-(const _Vector<T, N> &left, const _Vector<T, N> &right) noexcept
+[[nodiscard]] constexpr inline Vector<T, N> operator-(const Vector<T, N> &left, const Vector<T, N> &right) noexcept
+    requires Subtractable<T, T>
 {
     return Elementwise(left.elements, right.elements, std::minus<T>{});
 }
 
 template <typename T, size_t N>
-[[nodiscard]] constexpr inline _Vector<T, N> operator-(const _Vector<T, N> &left, const T &right) noexcept
+[[nodiscard]] constexpr inline Vector<T, N> operator-(const Vector<T, N> &left, const T &right) noexcept
+    requires Subtractable<T, T>
 {
     return Elementwise(left.elements, right, std::minus<T>{});
 }
 
 template <typename T, size_t N>
-[[nodiscard]] constexpr inline _Vector<T, N> operator-(const T &left, const _Vector<T, N> &right) noexcept
+[[nodiscard]] constexpr inline Vector<T, N> operator-(const T &left, const Vector<T, N> &right) noexcept
+    requires Subtractable<T, T>
 {
     return Elementwise(left, right.elements, std::minus<T>{});
 }
 
-template <typename T, size_t N> [[nodiscard]] constexpr inline _Vector<T, N> operator-(const _Vector<T, N> &v) noexcept
+template <typename T, size_t N>
+[[nodiscard]] constexpr inline Vector<T, N> operator-(const Vector<T, N> &v) noexcept
+    requires Negatable<T>
 {
     using namespace std;
     auto out = v;
@@ -157,109 +232,36 @@ template <typename T, size_t N> [[nodiscard]] constexpr inline _Vector<T, N> ope
 }
 
 template <typename T, size_t N>
-[[nodiscard]] constexpr inline _Vector<T, N> operator*(const _Vector<T, N> &left, T right) noexcept
+[[nodiscard]] constexpr inline Vector<T, N> operator*(const Vector<T, N> &left, T right) noexcept
+    requires Multiplicable<T, T>
 {
     return Elementwise(left.elements, right, std::multiplies<T>{});
 }
 
 template <typename T, size_t N>
-[[nodiscard]] constexpr inline _Vector<T, N> operator*(T left, const _Vector<T, N> &right) noexcept
+[[nodiscard]] constexpr inline Vector<T, N> operator*(T left, const Vector<T, N> &right) noexcept
+    requires Multiplicable<T, T>
 {
     return Elementwise(left, right.elements, std::multiplies<T>{});
 }
 
 template <typename T, size_t N>
-[[nodiscard]] constexpr inline _Vector<T, N> operator/(const _Vector<T, N> &numerator, T denominator) noexcept
+[[nodiscard]] constexpr inline Vector<T, N> operator/(const Vector<T, N> &numerator, T denominator) noexcept
+    requires Divisable<T, T>
 {
     return Elementwise(numerator.elements, denominator, std::divides<T>{});
 }
 
 template <typename T, size_t N>
-[[nodiscard]] constexpr inline T squared_distance(const _Vector<T, N> &from, const _Vector<T, N> &to) noexcept
+[[nodiscard]] constexpr inline T squared_distance(const Vector<T, N> &from, const Vector<T, N> &to) noexcept
+    requires std::floating_point<T>
 {
     return (from - to).squared_length();
 }
 
 template <typename T, size_t N>
-[[nodiscard]] constexpr inline T distance(const _Vector<T, N> &from, const _Vector<T, N> &to) noexcept
+[[nodiscard]] constexpr inline T distance(const Vector<T, N> &from, const Vector<T, N> &to) noexcept
+    requires std::floating_point<T>
 {
     return std::sqrt(squared_distance(from, to));
 }
-
-template <typename T, std::size_t N> class Vector : public _Vector<T, N>
-{
-  public:
-    using _Vector<T, N>::_Vector;
-};
-
-template <typename T> class Vector<T, 2> : public _Vector<T, 2>
-{
-  public:
-    using _Vector<T, 2>::_Vector;
-
-    [[nodiscard]] constexpr inline T x() const noexcept
-    {
-        return this->elements[0];
-    }
-
-    [[nodiscard]] constexpr inline T y() const noexcept
-    {
-        return this->elements[1];
-    }
-};
-
-template <typename T> class Vector<T, 3> : public _Vector<T, 3>
-{
-  public:
-    using _Vector<T, 3>::_Vector;
-
-    [[nodiscard]] constexpr inline T x() const noexcept
-    {
-        return this->elements[0];
-    }
-
-    [[nodiscard]] constexpr inline T y() const noexcept
-    {
-        return this->elements[1];
-    }
-
-    [[nodiscard]] constexpr inline T z() const noexcept
-    {
-        return this->elements[2];
-    }
-
-    [[nodiscard]] constexpr inline Vector cross(const Vector &v) const noexcept
-    {
-        const auto &u = *this;
-        const auto x = u.y() * v.z() - u.z() * v.y();
-        const auto y = u.z() * v.x() - u.x() * v.z();
-        const auto z = u.x() * v.y() - u.y() * v.x();
-        return {x, y, z};
-    }
-};
-
-template <typename T> class Vector<T, 4> : public _Vector<T, 4>
-{
-  public:
-    using _Vector<T, 4>::_Vector;
-
-    [[nodiscard]] constexpr inline T x() const noexcept
-    {
-        return this->elements[0];
-    }
-
-    [[nodiscard]] constexpr inline T y() const noexcept
-    {
-        return this->elements[1];
-    }
-
-    [[nodiscard]] constexpr inline T z() const noexcept
-    {
-        return this->elements[2];
-    }
-
-    [[nodiscard]] constexpr inline T w() const noexcept
-    {
-        return this->elements[3];
-    }
-};

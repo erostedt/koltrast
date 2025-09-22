c5edfb9e2a345c19d07614d2aa04e7dd22e8852a
Eric Rostedt <rostedteric@gmail.com>
2025-09-22 11:23:15 +0200

    move out reading of texture

--- examples/mesh/main.cpp
+++ examples/mesh/main.cpp
@@ -8,35 +8,9 @@
 #include "matrix.hpp"
 #include "obj.hpp"
 #include "rasterizer.hpp"
+#include "texture.hpp"
 #include "transform.hpp"
 #include "types.hpp"
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
-namespace fs = std::filesystem;
-
-Image<RGB<f32>> load_texture(const fs::path &path)
-{
-    CHECK(fs::exists(path));
-    stbi_set_flip_vertically_on_load(1);
-    int w, h, c;
-    u8 *data = stbi_load(path.c_str(), &w, &h, &c, STBI_rgb);
-    CHECK(data != NULL);
-    CHECK(c == 3);
-    Image<RGB<f32>> texture(static_cast<size_t>(w), static_cast<size_t>(h));
-
-    size_t size = (size_t)w * (size_t)h;
-    for (size_t i = 0; i < size; ++i)
-    {
-        auto &e = texture[i];
-        e.r = (f32)data[3 * i + 0] / 255.0f;
-        e.g = (f32)data[3 * i + 1] / 255.0f;
-        e.b = (f32)data[3 * i + 2] / 255.0f;
-    }
-    free(data);
-    return texture;
-}
 
 int main(int argc, char **argv)
 {
